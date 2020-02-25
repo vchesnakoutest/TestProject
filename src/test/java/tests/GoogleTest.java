9064c5ab7cf10998d44bbc0d4755c1d4c6730707
Dima Rak <Dima.Rak@oakam.com>
2020-02-25 12:43:56 +0300

    Added proper chromedriver

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -1,16 +1,13 @@
 package tests;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Issue;
-import io.qameta.allure.Link;
-import io.qameta.allure.TmsLink;
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 import tests.base.BaseTest;
 
 public class GoogleTest extends BaseTest {
     
     @Test(description = "Search in Google")
-    @Description("Validation of search functionality in Google")
+    @Description("Validation {model} of search functionality in Google")
     @Link("https://instagram.com/dmitryrak11")
     @Issue("dmitryrak11")
     @TmsLink("dmitryrak11")
