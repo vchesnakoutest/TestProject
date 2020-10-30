ef3987c014c6a705c89a80f1e1e8f7ce22ffafd2
Dmitry Rak <dmitryrak11@gmail.com>
2020-10-30 16:18:39 +0300

    Reformatted GooglePage

--- src/test/java/pages/GooglePage.java
+++ src/test/java/pages/GooglePage.java
@@ -3,14 +3,11 @@ package pages;
 import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import utils.AllureUtils;
 
 @Log4j2
-public class GooglePage extends BasePage{
+public class GooglePage extends BasePage {
 
     public GooglePage(WebDriver driver) {
         super(driver);
